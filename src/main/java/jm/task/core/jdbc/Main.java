aeb9f9224042b69a8d90a9abad237aa9b363c669
artemkacevic <akatsevich@live.ru>
2023-06-22 21:02:31 +0800

    Connection with DB works

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,13 @@
 package jm.task.core.jdbc;
 
+import java.sql.Connection;
+import java.sql.Driver;
+
 public class Main {
+    private static final String URL = "";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
     }
 }
