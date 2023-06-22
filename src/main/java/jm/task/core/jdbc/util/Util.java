1cc02c8f4749dd15dfefe1a21e75ffa700c9480f
artemkacevic <akatsevich@live.ru>
2023-06-22 23:07:58 +0800

    Connection with DB fixed in Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/?user=root";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Kat$evich!1324";
+    public static void connectToDB() {
+        System.out.println("Connecting database...");
+
+        try  {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            if (!connection.isClosed()){
+                System.out.printf("Database connected!");
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
+
+    }
 }
