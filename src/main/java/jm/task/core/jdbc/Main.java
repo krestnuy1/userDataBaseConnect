6b2d5e626ebbac0f52aa83ce2cec6ad4fd8706bf
artemkacevic <akatsevich@live.ru>
2023-06-22 21:11:08 +0800

    Update pom -> new JDBC Driver implemented Connection with DB fixed

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,13 +1,24 @@
 package jm.task.core.jdbc;
 
 import java.sql.Connection;
-import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
-    private static final String URL = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/?user=root";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "Kat$evich!1324";
     public static void main(String[] args) {
+
+        System.out.println("Connecting database...");
+
+        try  {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Database connected!");
+            connection.close();
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
 
     }
 }
