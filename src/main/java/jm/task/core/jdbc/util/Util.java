e5cc0b353c5643d4eb063a6c8eadd54f26714531
artemkacevic <akatsevich@live.ru>
2023-07-13 13:32:24 +0800

    Added logger into util;

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,13 @@
 package jm.task.core.jdbc.util;
 
 
+import org.slf4j.LoggerFactory;
+
 import java.sql.*;
 
 
 public class Util {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Util.class);
     private static final String URL = "jdbc:mysql://localhost:3306/UsersDB";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
@@ -13,9 +16,9 @@ public class Util {
     public Util() {
         try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            System.out.println("Успешное подключение к базе данных");
+            logger.info("Успешное подключение к базе данных");
         } catch (SQLException e) {
-            System.out.println("Не удалось подключиться к базе данных");
+            logger.warn("Не удалось подключиться к базе данных");
         }
     }
 
